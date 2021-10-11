9c5a571b67f6ea22b4a9389af16fed3d1dde63ba
Shogo Ohta <athos0220@gmail.com>
2021-10-11 20:42:03 +0900

    Dedupe consecutive invoke/invokeStatic rather than removing invoke

--- src/stacktracer/repl.clj
+++ src/stacktracer/repl.clj
@@ -1,9 +1,26 @@
 (ns stacktracer.repl
   (:require [stacktracer.core :as st]))
 
+(defn- dedupe-by [f]
+  (fn [rf]
+    (let [pv (volatile! ::none)]
+      (fn
+        ([] (rf))
+        ([result] (rf result)) 
+        ([result input]
+         (let [v (f input)
+               prior @pv]
+           (vreset! pv v)
+           (if (= prior v)
+             result
+             (rf result input))))))))
+
 (def default-xform
   (comp (remove #(re-find #"^clojure\.(:?core|main)\$" (name (:class %))))
-        (remove #(= (:method %) 'invoke))))
+        (dedupe-by (juxt :class :method :file :line))
+        (dedupe-by (juxt :class
+                         (fn [{m :method}]
+                           (get '{invoke invokeStatic} m m))))))
 
 (def ^:private default-options
   (atom {:xform default-xform :lines 7 :color true}))
