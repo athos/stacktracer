cde0d7080f157835bdbac6704b7395794df9c8f9
Shogo Ohta <athos0220@gmail.com>
2021-10-16 23:51:45 +0900

    Move dedupe-by to stacktracer.xforms

--- src/stacktracer/repl.clj
+++ src/stacktracer/repl.clj
@@ -1,28 +1,15 @@
 (ns stacktracer.repl
-  (:require [stacktracer.core :as st]))
-
-(defn- dedupe-by [f]
-  (fn [rf]
-    (let [pv (volatile! ::none)]
-      (fn
-        ([] (rf))
-        ([result] (rf result)) 
-        ([result input]
-         (let [v (f input)
-               prior @pv]
-           (vreset! pv v)
-           (if (= prior v)
-             result
-             (rf result input))))))))
+  (:require [stacktracer.core :as st]
+            [stacktracer.xforms :as sx]))
 
 (def default-xform
   (comp (remove #(re-matches #"(?:clojure|nrepl)\..*" (:fn %)))
-        (dedupe-by (juxt :fn :method :file :line))
-        (dedupe-by (juxt :class
-                         (fn [{m :method}]
-                           (get '{invoke invokeStatic
-                                  doInvoke invokeStatic}
-                                m m))))))
+        (sx/dedupe-by (juxt :fn :method :file :line))
+        (sx/dedupe-by (juxt :class
+                            (fn [{m :method}]
+                              (get '{invoke invokeStatic
+                                     doInvoke invokeStatic}
+                                   m m))))))
 
 (def ^:private default-options
   (atom {:xform default-xform :lines 7 :color true}))
--- src/stacktracer/xforms.clj
+++ src/stacktracer/xforms.clj
@@ -14,3 +14,17 @@
          ([result input]
           (vswap! buf conj input)
           result))))))
+
+(defn dedupe-by [f]
+  (fn [rf]
+    (let [pv (volatile! ::none)]
+      (fn
+        ([] (rf))
+        ([result] (rf result))
+        ([result input]
+         (let [v (f input)
+               prior @pv]
+           (vreset! pv v)
+           (if (= prior v)
+             result
+             (rf result input))))))))
