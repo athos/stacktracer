dd846199ec39cec8cec328f62f9109cf25c949e3
Shogo Ohta <athos0220@gmail.com>
2021-10-22 00:02:24 +0900

    Move IPrinter to protocols namespace

--- src/stacktracer/protocols.clj
+++ src/stacktracer/protocols.clj
@@ -1,4 +1,10 @@
-(ns stacktracer.protocols)
+(ns stacktracer.protocols
+  (:refer-clojure :exclude [print newline]))
+
+(defprotocol IPrinter
+  (print [this text])
+  (newline [this])
+  (with-alert [this alert f]))
 
 (defprotocol IRenderer
   (render-start [this e])
