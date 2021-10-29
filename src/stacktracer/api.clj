08a0bdb1a3af7194d3609426e732bcc4738e0890
Shogo Ohta <athos0220@gmail.com>
2021-10-29 22:19:23 +0900

    Support :include/:exclude as command-line args

--- src/stacktracer/api.clj
+++ src/stacktracer/api.clj
@@ -1,7 +1,13 @@
 (ns stacktracer.api
   (:require [clojure.java.io :as io]
             [stacktracer.reformat :as reformat])
-  (:import [java.io PushbackReader]))
+  (:import [java.io PushbackReader]
+           [java.util.regex Pattern]))
+
+(defn- fixup-opts [{:keys [include exclude] :as opts}]
+  (cond-> opts
+    include (assoc :include (Pattern/compile (str include)))
+    exclude (assoc :exclude (Pattern/compile (str exclude)))))
 
 (defn reformat [{:keys [from file] :or {from :guess} :as opts}]
   (with-open [r (PushbackReader. (io/reader (or file *in*)))]
@@ -9,7 +15,7 @@
                    (or (reformat/guess-format r)
                        :java-stacktrace)
                    (keyword from))
-          args (apply concat opts)]
+          args (apply concat (fixup-opts opts))]
       (case format
         :report-edn
         (apply reformat/reformat-report-edn r args)
