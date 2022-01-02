4c1c33808509e44054e558743e057258e41b647c
Shogo Ohta <athos0220@gmail.com>
2022-01-02 18:52:36 +0900

    Tweak formatting of compact renderer

--- src/stacktracer/renderer/compact.clj
+++ src/stacktracer/renderer/compact.clj
@@ -3,20 +3,26 @@
             [stacktracer.protocols :as proto]
             [stacktracer.renderer.common :as common]))
 
-(defn- render-trace-element [{:keys [printer]} elem]
-  (proto/with-color-type printer :info
-    #(common/printf printer "[%d/%d] %s:%d "
-                    (:id elem) (:total elem) (:file elem) (:line elem)))
-  (doto printer
-    (proto/print (str/trim (:focused elem)))
-    (proto/newline)))
+(defn- render-trace-elements [{:keys [printer]} elems]
+  (let [idigits (count (str (apply max (map :id elems))))
+        ldigits (count (str (apply max (map :line elems))))
+        file-max-len (apply max (map (comp count :file) elems))]
+    (doseq [elem elems]
+      (proto/with-color-type printer :info
+        #(common/printf printer "[%s/%d] %s:%s| "
+                        (common/pad idigits (:id elem))
+                        (:total elem)
+                        (common/pad file-max-len (:file elem))
+                        (common/pad ldigits (:line elem))))
+      (doto printer
+        (proto/print (str/trim (:focused elem)))
+        (proto/newline)))))
 
 (defrecord CompactRenderer [printer opts]
   proto/IRenderer
   (render-trace [this e elems]
     (when (and (:show-message opts) (not (:reverse opts)))
       (common/render-error-message printer e))
-    (doseq [elem elems]
-      (render-trace-element this elem))
+    (render-trace-elements this elems)
     (when (and (:show-message opts) (:reverse opts))
       (common/render-error-message printer e))))
